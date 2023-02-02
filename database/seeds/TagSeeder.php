50f0de138bec86cf0fc995f4f059ab895762c5b4
SimoCrash <simone.crescenzi@hotmail.com>
2023-02-02 20:02:43 +0100

    fine giornata

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['PHP', 'Laravel', 'VueJs', 'Cucina Moderna', 'Piatti Tipici', 'Roma', 'Venezia', 'Emilia-Romagna', 'Acqua Minerale', 'Valle d\'Aosta'];
+    }
+}
